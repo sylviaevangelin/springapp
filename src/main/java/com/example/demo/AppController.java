f95cd593abbc004bf263737ad66cabe829f5f537
sylviaevangelin3 <sylviaevangelin3@gmail.com>
2022-11-25 10:29:00 +0530

    sylvia

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+	
+	
+	
+	@GetMapping("home")
+	public String getHome() {
+		
+		return "Welcome home";
+	}
+
+}
